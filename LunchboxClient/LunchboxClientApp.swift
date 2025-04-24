f181f1a06acd4de5d02db9b6841ed523f9f4a213
Ambrose <ambymbayi45@gmail.com>
2025-04-24 09:39:12 +0300

    Initial Commit

---
+++ LunchboxClient/LunchboxClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchboxClientApp.swift
+//  LunchboxClient
+//
+//  Created by Ambrose Mbayi on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LunchboxClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
