3d60b40f6a49056b8a9c7d255d1011b0153fcdc7
Ambrose <ambymbayi45@gmail.com>
2025-04-27 13:10:50 +0300

    Added other Connects to ThemeConfiguration Model and Response

--- LunchboxClient/Models/ClientConfiguration/ThemeConfiguration.swift
+++ LunchboxClient/Models/ClientConfiguration/ThemeConfiguration.swift
@@ -10,8 +10,30 @@ struct ThemeConfiguration: Codable {
             let description: String?
         }
         
+        struct ThemeImages: Codable {
+            let logoUrl: String?
+            let wordmark: String?
+            let startOrderPageImage: String?
+            let giftCardImage: String?
+        }
+        
+        struct PopUp: Codable {
+            let body: String?
+            let header: String?
+            let enable: Bool?
+            let showOnce: Bool?
+            let imageUrl: String?
+        }
+        
+        let themeImages: ThemeImages?
         let hubMarketing: [HubMarketing]?
+        let popUp: PopUp?
     }
     
+    var id: String {
+        return tenantId
+    }
+    
+    let tenantId: String
     let settings: Settings?
 }
--- LunchboxClient/Network/Requests/Theme Configuration/GetThemeConfigurationResponse.swift
+++ LunchboxClient/Network/Requests/Theme Configuration/GetThemeConfigurationResponse.swift
@@ -5,7 +5,33 @@ struct GetThemeConfigurationResponse: Decodable {
 }
 
 struct AssetData: Codable {
+    let tenant: String?
+    let app_name: String
+    let app_icon: AssetAppIcon
+    let images: AssetImages?
     let hub_marketing: [AssetHubMarketing]?
+    let pop_up: AssetPopUp?
+}
+
+struct AssetAppIcon: Codable {
+    let background: URL?
+    let foreground: URL?
+    let composite: URL?
+}
+
+struct AssetImages: Codable {
+    let logo: String?
+    let wordmark: String?
+    let gift_card_image: String?
+    let start_order_page_image: String?
+}
+
+struct AssetPopUp: Codable {
+    let body: String?
+    let header: String?
+    let enable: Bool?
+    let show_once: Bool?
+    let image: String?
 }
 
 struct AssetHubMarketing: Codable {
@@ -19,13 +45,43 @@ struct AssetHubMarketing: Codable {
 
 extension ThemeConfiguration {
     init(response: AssetData?) {
+        self.tenantId = response?.tenant ?? ""
         self.settings = Settings(response: response)
     }
 }
 
 extension ThemeConfiguration.Settings {
     init(response: AssetData?) {
+        
+        if let themeImages = response?.images {
+            self.themeImages = ThemeImages(response: themeImages)
+        } else {
+            self.themeImages = nil
+        }
+        
         self.hubMarketing = response?.hub_marketing?.compactMap {HubMarketing(response: $0)}
+        self.popUp = PopUp(response: response?.pop_up)
+        
+    }
+}
+
+extension ThemeConfiguration.Settings.ThemeImages {
+    init(response: AssetImages) {
+        self.logoUrl = response.logo
+        self.startOrderPageImage = response.start_order_page_image
+        self.giftCardImage = response.gift_card_image
+        self.wordmark = response.wordmark
+    }
+}
+
+extension ThemeConfiguration.Settings.PopUp {
+    init(response: AssetPopUp?) {
+        self.body = response?.body
+        self.header = response?.header
+        self.enable = response?.enable
+        self.showOnce = response?.show_once
+        self.imageUrl = response?.image
+    
     }
 }
 
